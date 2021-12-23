b4ecbbc1d4eeb070f00189131d230780790a61eb
Shyren <shyren.more30@gmail.com>
2021-12-23 16:52:35 +0530

    add hoare's partition

--- Sorting/7_quicksort_partition.cpp
+++ Sorting/7_quicksort_partition.cpp
@@ -59,6 +59,112 @@ void naive_partition(int arr[], int l, int h, int p)
         cout << arr[i] << " ";
 }
 
+/*
+
+Hoare's partition: O(n) time, O(1) time
+pivot is first element i.e pivot = arr[l]
+initialise i=low-1, j=high+1 
+
+maintain two windows:
+0 to i : el < pivot
+
+eg: arr[] = {5, 3, 8, 4, 2, 7, 1, 10}
+l=0, h=7
+pivot = arr[l] = 5
+
+begin:
+i=-1, j=8
+
+we move i from l to h, and move j from h to l, until i and j cross each other 
+
+for every iteration we ensure:
+1) el from l to i < pivot
+2) el from j to h > pivot 
+*/
+
+
+// returns index of partition
+int hoare_partition(int arr[], int low, int high)
+{
+    int pivot = arr[low];
+    int i = low - 1;
+    int j = high + 1;
+    while(true)
+    {
+        do{
+            ++i;
+        } while (arr[i] < pivot);
+
+        do{
+            --j;
+        } while (arr[j] > pivot);
+
+        if(i>=j)
+            return j;
+
+        swap(arr[i], arr[j]);
+    }
+}
+
+/*
+
+dry run:
+
+initially
+arr = 5, 3, 8, 4, 2, 7, 1, 10
+i=-1, j=8, pivot=5
+
+1st iter:
+i=0, 5<5 stop i, 
+
+j=7, 10>5
+j=6, 1>5 stop j
+
+swap(arr[0], arr[6])
+
+arr = 1, 3, 8, 4, 2, 7, 5, 10
+i=1, 3<5
+i=2, 8<5 stop i 
+
+j=5, 7>5
+j=4, 2>5 stop j
+
+swap arr[2] with arr[4]
+
+arr = 1, 3, 2, 4, 8, 7, 5, 10
+i=3, 4<5
+i=4, 8<5 stop
+
+j=3, 4>5 stop
+
+i has cross j, return 3
+at idx=3, el at right side are greater than pivot
+el at left are smaller than pivot
+*/
+
+/*
+
+Corner case: pivot is smallest
+arr = 5, 10, 9, 12
+i=0, 5<5 stop
+j=3, 12>5
+j=2,..
+j=0, 5>5 stop j
+
+i==j
+return 0
+arr is partitioned into 5 | 10, 9, 12
+
+*/
+
+/*
+
+Hoare's partition is not stable
+proof: arr = 5,5,5,5
+i stop at 0 and j at 3, swapping happens, khatam tata bye bye
+
+Do note, naive method is stable but requires O(n) extra space and 3 traversals
+*/
 int main()
 {
     int n;
