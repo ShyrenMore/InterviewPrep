4492aca000773e7320cdadc36afd49a81389f8f1
Shyren <shyren.more30@gmail.com>
2022-01-28 18:19:25 +0530

    saving sample ip/op

---
+++ LinkedList/intersectionPoint.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * Given two LL, both of them have some common nodes 
+ * there is one node where they intersect after which they have all nodes in common
+ * Find intersection point 
+ * 
+ * ip: 
+ * h1 -> 5->8->7-->10->12->15->null
+ *                  ^
+ *                  |
+ *         h2 -> 9 ->
+ * 
+ * op: 10
+ */
+
+public class intersectionPoint {
+    
+}
