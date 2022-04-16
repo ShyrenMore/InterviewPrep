28d87a83991f1bb4df65f63cb1850e2e853d36ed
Shyren <shyren.more30@gmail.com>
2022-04-16 23:57:32 +0530

    Add April Lunchtime

---
+++ TP/april lunchtime/1.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+#define deb(x) cout << #x << ": " << x << endl;
+#define deb2(x, y) cout << #x << ": " << x << " ~ " << #y << ": " << y << endl;
+#define in(n, arr)              \
+    for (int i = 0; i < n; i++) \
+    cin >> arr[i]
+#define out_arr(n, arr)         \
+    for (int i = 0; i < n; i++) \
+    cout << arr[i]
+#define op(n) cout << n << "\n"
+#define lli long long int
+#define here cout << "\nHERE\n"
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+using namespace std;
+
+// function returns unique prime factors
+// eg: 30 has factors 2, 3, 5, 6, 10, 15, so function will return only 3 (2, 3, 5)
+lli countUniquePrimeFactors(lli n)
+{
+    lli cnt = 0;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            ++cnt;
+            // we don't want multiples of say 2 or 3..
+            while (!(n % i))
+                n /= i;
+        }
+    }
+
+    // if n is prime
+    if (n != 1)
+        ++cnt;
+
+    return cnt;
+}
+
+int main()
+{
+
+    // /*
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    // */
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        lli n;
+        cin >> n;
+        vector<lli> arr(n);
+        in(n, arr);
+
+        
+        lli noOfOne = 0, noOfTwo = 0;
+        for(auto x: arr)
+        {
+            if(x==1)
+                noOfOne++;
+            
+            if(x==2)
+                noOfTwo++;
+        }
+
+        lli ans = noOfOne * (n - noOfOne);
+
+        if (noOfOne>1)
+            ans += ((noOfOne * (noOfOne - 1)) / 2);
+        
+        if(noOfTwo > 1)
+            ans += ((noOfTwo * (noOfTwo - 1)) / 2);
+
+        op(ans);
+    }
+
+    return 0;
+}
